b6e69e35128b0d412483757a3921bcd472cef9ef
chris-cozy <csande9@clemson.edu>
2021-10-21 20:19:23 -0400

    Added linked list code snippet

---
+++ linked_list.c
@@ -0,0 +1,284 @@
+// A simple C program to introduce
+// a linked list
+#include <stdio.h>
+#include <stdlib.h>
+
+// A linked list node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// This function prints contents of linked list starting from
+// the given node
+void printList(struct Node* n)
+{
+    while (n != NULL) {
+        printf(" %d ", n->data);
+        n = n->next;
+    }
+}
+
+/* Given a reference (pointer to pointer) to the head of a list and
+   an int, inserts a new node on the front of the list. */
+void push(struct Node** head_ref, int new_data)
+{
+    /* 1. allocate node */
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+ 
+    /* 2. put in the data  */
+    new_node->data  = new_data;
+ 
+    /* 3. Make next of new node as head */
+    new_node->next = (*head_ref);
+ 
+    /* 4. move the head to point to the new node */
+    (*head_ref)    = new_node;
+}
+ 
+/* Given a node prev_node, insert a new node after the given
+   prev_node */
+void insertAfter(struct Node* prev_node, int new_data)
+{
+    /*1. check if the given prev_node is NULL */
+    if (prev_node == NULL)
+    {
+      printf("the given previous node cannot be NULL");
+      return;
+    }
+ 
+    /* 2. allocate new node */
+    struct Node* new_node =(struct Node*) malloc(sizeof(struct Node));
+ 
+    /* 3. put in the data  */
+    new_node->data  = new_data;
+ 
+    /* 4. Make next of new node as next of prev_node */
+    new_node->next = prev_node->next;
+ 
+    /* 5. move the next of prev_node as new_node */
+    prev_node->next = new_node;
+}
+ 
+/* Given a reference (pointer to pointer) to the head
+   of a list and an int, appends a new node at the end  */
+void append(struct Node** head_ref, int new_data)
+{
+    /* 1. allocate node */
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    struct Node *last = *head_ref;  /* used in step 5*/
+ 
+    /* 2. put in the data  */
+    new_node->data  = new_data;
+ 
+    /* 3. This new node is going to be the last node, so make next of
+          it as NULL*/
+    new_node->next = NULL;
+ 
+    /* 4. If the Linked List is empty, then make the new node as head */
+    if (*head_ref == NULL)
+    {
+       *head_ref = new_node;
+       return;
+    }
+ 
+    /* 5. Else traverse till the last node */
+    while (last->next != NULL)
+        last = last->next;
+ 
+    /* 6. Change the next of last node */
+    last->next = new_node;
+    return;
+}
+
+/* Given a reference (pointer to pointer) to the head of a
+   list and an int, inserts a new node on the front of the
+   list. */
+void push(struct Node** head_ref, int new_data)
+{
+    struct Node* new_node
+        = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+ 
+/* Given a reference (pointer to pointer) to the head of a
+   list and a key, deletes the first occurrence of key in
+   linked list */
+void deleteNode(struct Node** head_ref, int key)
+{
+    // Store head node
+    struct Node *temp = *head_ref, *prev;
+ 
+    // If head node itself holds the key to be deleted
+    if (temp != NULL && temp->data == key) {
+        *head_ref = temp->next; // Changed head
+        free(temp); // free old head
+        return;
+    }
+ 
+    // Search for the key to be deleted, keep track of the
+    // previous node as we need to change 'prev->next'
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+ 
+    // If key was not present in linked list
+    if (temp == NULL)
+        return;
+ 
+    // Unlink the node from linked list
+    prev->next = temp->next;
+ 
+    free(temp); // Free memory
+}
+
+/* Given a reference (pointer to pointer) to the head of a list
+   and a position, deletes the node at the given position */
+void deleteNodeAtPos(struct Node **head_ref, int position)
+{
+   // If linked list is empty
+   if (*head_ref == NULL)
+      return;
+ 
+   // Store head node
+   struct Node* temp = *head_ref;
+ 
+    // If head needs to be removed
+    if (position == 0)
+    {
+        *head_ref = temp->next;   // Change head
+        free(temp);               // free old head
+        return;
+    }
+ 
+    // Find previous node of the node to be deleted
+    for (int i=0; temp!=NULL && i<position-1; i++)
+         temp = temp->next;
+ 
+    // If position is more than number of nodes
+    if (temp == NULL || temp->next == NULL)
+         return;
+ 
+    // Node temp->next is the node to be deleted
+    // Store pointer to the next of node to be deleted
+    struct Node *next = temp->next->next;
+ 
+    // Unlink the node from linked list
+    free(temp->next);  // Free memory
+ 
+    temp->next = next;  // Unlink the deleted node from list
+}
+
+/* Function to delete the entire linked list */
+void deleteList(struct Node** head_ref)
+{
+   /* deref head_ref to get the real head */
+   struct Node* current = *head_ref;
+   struct Node* next;
+ 
+   while (current != NULL)
+   {
+       next = current->next;
+       free(current);
+       current = next;
+   }
+   
+   /* deref head_ref to affect the real head back
+      in the caller. */
+   *head_ref = NULL;
+}
+
+/* Counts no. of nodes in linked list */ //iterative
+int getCount(struct Node* head)
+{
+    int count = 0;  // Initialize count
+    struct Node* current = head;  // Initialize current
+    while (current != NULL)
+    {
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+
+bool search(struct Node* head, int x)
+{
+    struct Node* current = head;  // Initialize current
+    while (current != NULL)
+    {
+        if (current->key == x)
+            return true;
+        current = current->next;
+    }
+    return false;
+}
+
+/* Checks whether the value x is present in linked list */ //recursive version
+bool search(struct Node* head, int x)
+{
+    // Base case
+    if (head == NULL)
+        return false;
+     
+    // If key is present in current node, return true
+    if (head->key == x)
+        return true;
+ 
+    // Recur for remaining list
+    return search(head->next, x);
+}
+
+// Takes head pointer of
+// the linked list and index
+// as arguments and return
+// data at index
+int GetNth(struct Node* head, int index)
+{
+ 
+    struct Node* current = head;
+ 
+    // the index of the
+    // node we're currently
+    // looking at
+    int count = 0;
+    while (current != NULL) {
+        if (count == index)
+            return (current->data);
+        count++;
+        current = current->next;
+    }
+ 
+    /* if we get to this line,
+       the caller was asking
+       for a non-existent element
+       so we assert fail */
+    assert(0);
+}
+
+int main()
+{
+    struct Node* head = NULL;
+    struct Node* second = NULL;
+    struct Node* third = NULL;
+ 
+    // allocate 3 nodes in the heap
+    head = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+ 
+    head->data = 1; // assign data in first node
+    head->next = second; // Link first node with second
+ 
+    second->data = 2; // assign data to second node
+    second->next = third;
+ 
+    third->data = 3; // assign data to third node
+    third->next = NULL;
+ 
+    printList(head);
+ 
+    return 0;
+}
