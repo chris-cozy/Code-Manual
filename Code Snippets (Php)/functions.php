9215fcb4966d900917a81e5802a28fbf3e82ad12
chris-cozy <csande9@clemson.edu>
2021-11-15 21:19:24 -0500

    added user functions

--- Code Snippets (Php)/functions.php
+++ Code Snippets (Php)/functions.php
@@ -5,8 +5,10 @@
         <title>Doc Title</title>
     </head>
     <body>
-        
-        <?php
+        <!--Variable scope:
+            Global scope: defined outside of the function
+            Local scope: defined inside the function -->
+         <?php
             //There are predefined and user defined functions, like in C
             //Example of predefined function use
             $name = "KAMORI";
@@ -21,6 +23,18 @@
             echo pos;
 
             $full_name = str_replace($name, "CHRIS", $full_name);
+
+            //USER DEFINED FUNCTIONS - keep them reusable
+            //basic setup - use the function tag to define as a function
+            //It's typical to include a separate document defining all of your php functions for the program
+            $y = 100;
+            function practice($x) 
+            {
+                $new_num = $x * 0.75;
+                echo "here is 75% of what you wrote ".$new_num;
+            }
+            practice($y);
+
         ?>
     </body>
 </html>
