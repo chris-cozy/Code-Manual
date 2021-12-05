8f3205be674d23ea792a55d7dbfaaca0c42218f1
chris-cozy <csande9@clemson.edu>
2021-12-05 18:51:41 -0500

    Added thread files

--- Code Snippets (C)/tokenizer.c
+++ Code Snippets (C)/tokenizer.c
@@ -3,6 +3,13 @@
 #include <string.h>
 
 //Tokenizes the string
+/*
+	strtok() - destroys the string that its parsing
+	Keeps a pointer to a position that it's at to 'save progress'
+	Can only tokenize one message at a time, 
+	so if you call it in one thread for msg1 then call it in another thread for msg2 before msg1 is finished, 
+	the next time the first thread calls strtok it will return the next token from msg2
+*/
 void token(char str[], char delim[]){
 	char *ptr = strtok(str, delim);
 	while(ptr != NULL)
