c7317e53c3ad769044eda176873fe951f35fcf62
chris-cozy <csande9@clemson.edu>
2022-05-15 21:18:29 -0400

    Added strings file

---
+++ Python/basics/strings.py
@@ -0,0 +1,9 @@
+#Strings use either single or double quotes
+test1 = 'Example string';
+test2 = "Example 'String'";
+
+#String indexing begins at 0
+letter0 = test1[0];
+
+#Quick way to reverse a string
+test1 = test1[::-1];
