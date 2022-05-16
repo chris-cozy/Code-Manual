010427c897575b3562aa6d1a92a22bdfa456ed31
chris-cozy <csande9@clemson.edu>
2022-05-16 13:56:14 -0400

    Added file on the 'in' operator

---
+++ Python/basics/operators/in.py
@@ -0,0 +1,9 @@
+# in - can be used to quickly check if an object is in a list, string, dict, tuple, etc.
+#Returns True or False
+'x' in [1,2,3]
+'x' in ['x', 'y', 'z']
+
+d = {'mykey': 345}
+345 in d.values()
+
+'mykey' in {'mykey': 345}
