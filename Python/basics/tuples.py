2c09d20f2e9e4a9f558697e0f8ffa1b8df344e77
chris-cozy <csande9@clemson.edu>
2022-05-16 11:40:26 -0400

    Added a file on tuples

---
+++ Python/basics/tuples.py
@@ -0,0 +1,11 @@
+#Tuples are essentially immutable lists and use parenthesis
+# mainly used for passing around objects in a program with 
+# confidence that the contents will not be changed, data integrity
+test = ('a', 'b', 'c', 'd', 'a')
+
+#Built-in tuple functions
+#count - returns number of occurences of that element
+#index - returns index of the element
+test.count('a')
+test.index('d')
+
