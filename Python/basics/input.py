2595084fc5c9e6a285e0ede53ce7cec3b6cc734d
chris-cozy <csande9@clemson.edu>
2022-05-16 14:03:59 -0400

    Added file for input function

---
+++ Python/basics/input.py
@@ -0,0 +1,6 @@
+#input() - Displays text then asks user for input. 
+# Typically saved under a variable for later callback. 
+# Anything passed into it by the user is taken as a string. 
+# Have to cast as int or float if you want the return variable as one of those types.
+test = input('Enter a number here: ')
+float(test)
