b943637f6d4a34b743f5d2c851bd450b4ceb0e82
chris-cozy <csande9@clemson.edu>
2022-05-16 13:55:48 -0400

    Reorganization

--- Python/basics/range.py
+++
@@ -1,7 +0,0 @@
-#The range function returns a number in the range specified
-for num in range(0,10,2):
-    print(num)
-
-
-#Range is a generator, so if you want a list of the generated values you must type-cast it as a list.
-list(range(0,11,2))
