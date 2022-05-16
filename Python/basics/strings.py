32f69212d404afe77a63ebe51a4f7f1905dadd2a
chris-cozy <csande9@clemson.edu>
2022-05-16 11:22:06 -0400

    Added string data

--- Python/basics/strings.py
+++ Python/basics/strings.py
@@ -1,19 +1,26 @@
 #Strings use either single or double quotes
-test1 = 'Example string';
-test2 = "Example 'String'";
+test1 = 'Example string'
+test2 = "Example 'String'"
 
 #String indexing begins at 0
-letter0 = test1[0];
+letter0 = test1[0]
 
 #String slicing [start:stop:step], step is size of the jumps you take from start to stop. the stop index is not inclusive
-slice1 = test1[0::2];
+slice1 = test1[0::2]
 
 #Quick way to reverse a string
-test1_r = test1[::-1];
+test1_r = test1[::-1]
 
 #Built-in string functions for upper and lower case. Have to reassign if want to use the return
-test1.upper();
-test2.lower();
+test1.upper()
+test2.lower()
 
 #Built-in string function for splitting string into a list based on a character. Reassign if you want to use return
-' '.split(test1);
+' '.split(test1)
+
+#Strings are immutable and don't allow item assignment
+#Two methods of inserting a variable into string: f-strings, and .format()
+name = 'Chris'
+print(f'Hello, my name is {name}')
+
+print('This is a {} string'.format('test'))
