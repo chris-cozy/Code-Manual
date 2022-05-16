2ebf13172cd43f8e865453a29e160f25a6fe66ec
chris-cozy <csande9@clemson.edu>
2022-05-16 11:28:01 -0400

    Created a file for python lists

---
+++ Python/basics/lists.py
@@ -0,0 +1,14 @@
+#Lists are essentially arrays
+test_list = [1,2,3,4,5]
+
+#Built-in list functions. These have no return, they change the actual list
+#sort - sorts in alphabetical or numerical order
+#reverse - reverses list
+test_list.sort()
+test_list.reverse()
+
+#Lists support indexing, slicing, and reassingment
+#Built-in list functions.
+#append adds to end of list, pop removes from end of list and returns removed item
+test_list.append(6)
+test_list.pop()
