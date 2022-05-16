acebdd981ea4ea96a045d55ac329b7477fed3252
chris-cozy <csande9@clemson.edu>
2022-05-16 13:46:28 -0400

    Updated file

--- Python/basics/strings.py
+++ Python/basics/strings.py
@@ -25,6 +25,12 @@ print(f'Hello, my name is {name}')
 
 print('This is a {} string'.format('test'))
 
+#Another example of format()
+index_count = 0
+for letter in 'abcde':
+    print('At index {} the letter is {}'.format(index_count,letter))
+    index_count += 1
+
 #String concatenation is easy
 test3 = 'Hello'
 test4 = 'World'
