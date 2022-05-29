cde0909398de0fb4d69165ce5eafeb4ddfd72ae6
chris-cozy <csande9@clemson.edu>
2022-05-29 13:25:46 -0400

    Added while loop information

---
+++ C/basics/statements/while_loop.c
@@ -0,0 +1,17 @@
+//Basic syntax
+while (condition)
+{
+    statement(s);
+}
+
+//Example - loop iterates until num is equal to 10
+int test = true;
+int num = 0;
+while(test)
+{
+    num++;
+    if(num == 10)
+    {
+        test = false;
+    }
+}
