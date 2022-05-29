db85f52072438982cdacbf9b64f47c007d752d40
chris-cozy <csande9@clemson.edu>
2022-05-29 13:10:04 -0400

    Added for-loop notes

---
+++ C/basics/statements/for_loop.c
@@ -0,0 +1,11 @@
+//Basic syntax
+for(init ; condition; increment)
+{
+    statement(s);
+}
+
+//Example - This iterates 10 times and prints the value of the iterator
+for(int i = 0; i < 10; i++)
+{
+    printf("Value of the iterator: %d", i);
+}
