25eb94a96011f949a54c2c51264c89443318a6f7
chris-cozy <csande9@clemson.edu>
2022-06-21 15:54:42 -0400

    Added simple webscraping example

--- Python/web_scraping/webscraping.py
+++ Python/web_scraping/webscraping.py
@@ -1,3 +1,22 @@
+# Webscraping example script
+# Purpose: Grab a page title
 import requests
 import bs4
+
+# Simple GET request
+# .text method
+    # Returns the page's source code as a string
+result = requests.get("http://example.com")
+
+# Using beautiful soup to parse the response
+# Passing in the string and what engine we will be using, in this case: lxml
+    # Returns a soup object
+soup = bs4.BeautifulSoup(result.text,"lxml")
+
+# You can print 'soup' and it will return the source code string, reformatted to match
+# the original HTLM indexing
+print(soup)
+
+# select() - this is how you access the HTML elements
+soup.select('title')
 
