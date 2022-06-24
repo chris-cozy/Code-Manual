1af95f6ede10f2a3e271851b006d6c57eec598d1
chris-cozy <csande9@clemson.edu>
2022-06-24 12:06:15 -0400

    Updated to select all the books from the pages

--- Python/web_scraping/multiple_pages.py
+++ Python/web_scraping/multiple_pages.py
@@ -1,3 +1,18 @@
 # Site designed to practice webscraping: www.toscrape.com
 # Webscraping example script
 # Purpose: Grabbing elements across multiple pages
+# Will be using the bookstore website
+# Get the title of every book with a 2-star rating
+import bs4
+import requests
+
+# We can alter the url accordingly to access separate pages.
+url = "https://books.toscrape.com/catalogue/page-{}.html"
+
+for num in range(1,50):
+    url.format(f'{num}')
+    result = requests.get(url)
+    soup = bs4.BeautifulSoup(result.text,"lxml")
+
+    choice = '.product_pod'
+    books = soup.select(choice)
