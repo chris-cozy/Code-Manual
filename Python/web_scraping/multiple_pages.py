d66fbdb48c64e2f77da224eda22eecc3a8e5ffb4
chris-cozy <csande9@clemson.edu>
2022-06-27 10:43:22 -0400

    Completed script

--- Python/web_scraping/multiple_pages.py
+++ Python/web_scraping/multiple_pages.py
@@ -6,14 +6,28 @@
 import bs4
 import requests
 
+two_star_titles = []
 # We can alter the url accordingly to access separate pages.
 # In this example, each page is associated with a page number
+# Be sure to assign the new url to a variable
 url = "https://books.toscrape.com/catalogue/page-{}.html"
 
-for num in range(1,50):
-    url.format(f'{num}')
-    result = requests.get(url)
+for num in range(1,51):
+    new_url = url.format(num)
+    result = requests.get(new_url)
     soup = bs4.BeautifulSoup(result.text,"lxml")
 
+    # Selecting all books(products) from the page
     choice = '.product_pod'
     books = soup.select(choice)
+
+    for book in books:
+        # Check if the book is 2-star
+        choice = '.star-rating.Two'
+        if len(book.select(choice)) != 0:
+            # Get title of book, and add to list
+                # In this example, the title is nested in the second link, under the title tag
+            title = book.select('a')[1]['title']
+            two_star_titles.append(title)
+
+print(two_star_titles)
