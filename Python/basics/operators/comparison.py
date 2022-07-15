35c731a6b50c68d57d1e473ec4e05e805142e245
chris-cozy <csande9@clemson.edu>
2022-07-15 11:20:56 -0400

    Added file on comparison operators

---
+++ Python/basics/operators/comparison.py
@@ -0,0 +1,21 @@
+# Comparison operators
+# If the values of the two operands are equal, the condition is true
+4 == 4
+
+# If the values of the two operands are not equal, the condition becomes true
+3 != 4
+
+# Greater than, Greater than or equal to
+# Less than, Less than or equal to
+5 > 4
+4 >= 4
+3 < 5
+6 <= 6
+
+
+# Chained comparison operators
+# You can chain multiple comparisons for more complex tests
+# You can use these as shorthand for larger Boolean Expressions
+1 < 2 < 3
+1 < 2 and 2 < 3
+1 > 2 or 3 == 3
